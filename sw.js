91c93b6d83dbece05a56e2f8e2b53acaa437174b
IagoGrangeiro <90574669+IagoGrangeiro@users.noreply.github.com>
2022-03-27 15:46:45 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-
+self.addEventListener('install', event => {
+  console.log('Install event!');
+});
+self.addEventListener('activate', event => {
+  console.log('Activate event!');
+});
+self.addEventListener('fetch', event =>{
+  console.log('Fetch intercepted for: ', event.request.url);
+});
